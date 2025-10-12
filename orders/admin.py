b91d89ebabe051f164f74b8bd573a4bdbabd3c8d
Bruno Ferreira da Silva <92815660+brunoF-Silva@users.noreply.github.com>
2025-10-12 01:02:31 -0300

    feat(orders): add order and orderitem models

--- orders/admin.py
+++ orders/admin.py
@@ -1,3 +1,17 @@
+'''Admin configuration for the orders app.'''
 from django.contrib import admin
+from . import models
 
-# Register your models here.
+class OrderItemInline(admin.TabularInline):
+    ''' Inline admin for OrderItem model '''
+    model = models.OrderItem
+    extra = 1
+    
+class OrderAdmin(admin.ModelAdmin):
+    ''' Admin for Order model '''
+    inlines = [
+        OrderItemInline,
+    ]
+
+admin.site.register(models.Order, OrderAdmin)
+admin.site.register(models.OrderItem)
