b2f8509b2b8ac474b3ab4f77090801e13aa29678
Bruno Silva <92815660+brunoF-Silva@users.noreply.github.com>
2025-10-16 23:07:15 -0300

    feat(cart): render the cart page
    This feature renders the shopping cart page, where users can view the items
    they've added to their cart, the individual amount to pay considering
    the number of times an item appears in the cart, and the total payable
    amount. Clients can also delete a group of variations from their cart.
    
    - Implements the `AddToCartView` to store product variations in the
    user's session-based shopping cart.
    - Implements the `RemoveFromCartView` to remove clusters of variations
    from the cart.
    - Creates the `cart` template to render information about the items
    selected.
    - Creates the utilities `cart_totals` and `cart_total_qty` to provide
    the total sum of the purchase and the total number of items in each
    variation cluster stored in the cart.
    - Creates tailored `cart_total_qty` and `cart_totals` filters for 
    template formatting.
    - Updates the `cart` icon in the navbar according to the current
    client's session.
    - Fixes the `cart` navbar rendering issue.
    - Creates empty templates for further implementation.
    - Creates an empty form for the `profiles` app.

--- utils/utils.py
+++ utils/utils.py
@@ -7,3 +7,23 @@ def format_price_usa(value):
 def format_price_br(value):
     '''Format a number as Brazilian currency.'''
     return f'R${value:.2f}'.replace('.', ',')
+
+def cart_total_qty(cart):
+    '''Return the total quantity of each item's variation in the cart'''
+    return sum([item['quantity'] for item in cart.values()])
+
+def cart_totals(cart):
+    '''Return the total amount to be paid in the cart.
+    
+    Return the total amount to be paid considering each  
+    item in the cart and whether they are on sale or not.
+    '''
+    return sum(
+        [
+            item.get('promotional_quantitative_pricing')
+            if item.get('promotional_quantitative_pricing')
+            else item.get('quantitative_pricing')
+            for item
+            in cart.values()
+        ]
+    )
