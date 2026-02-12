c5bfabc92e6e9e16e61681a4c1b5d1cc9693f263
Bruno Silva <92815660+brunoF-Silva@users.noreply.github.com>
2026-02-12 11:00:38 -0300

    feat: add place the order
    This commit adds the "place the order" feature, allowing users to make a valid
    purchase request.
    
    - `PlaceOrderView` assumed the functionality held by
    `PaymentDetailView`.
    - `PaymentDetailView` now secures that users can only see their own
    orders.
    - `PlaceOrderView` doesn't allow users to access the payment section
    when logged out or when the cart is empty. A client's cart will also
    auto-update the number of items in a request stored in the shopping cart
    based on available stock for those items within seven days, if users 
    finally decide to buy the items.

--- orders/urls.py
+++ orders/urls.py
@@ -4,7 +4,8 @@ from . import views
 app_name = 'order'
 
 urlpatterns = [
-    path('', views.PaymentView.as_view(), name="payment"),
+    path('pagar/<int:pk>', views.PaymentDetailView.as_view(), name="payment"),
     path('placeorder/', views.PlaceOrderView.as_view(), name="placeorder"),
+    path('list/', views.ListOrderView.as_view(), name="list"),
     path('detail/', views.OrderDetailView.as_view(), name="detail"),
 ]
--- orders/views.py
+++ orders/views.py
@@ -1,17 +1,130 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
 from django.views import View
-from django.views.generic import DetailView
+from django.views.generic import ListView, DetailView
 from django.http import HttpResponse
+from django.contrib import messages
 
-class PaymentView(View):
-    def get(self, *args, **kwargs):
-        return HttpResponse("Payment")
+from products.models import Variation
+from .models import Order, OrderItem
+
+from utils import utils
+
+class DispatchLoginRequired(View):
+    def dispatch(self, *args, **kwargs):
+        if not self.request.user.is_authenticated:
+            return redirect('profile:create')
+        
+        return super().dispatch(*args, **kwargs)
+    
+class PaymentDetailView(DispatchLoginRequired, DetailView):
+    template_name = 'order/payment.html'
+    model = Order
+    pk_url_kwarg = 'pk'
+    context_object_name = 'order'
+    
+    def get_queryset(self, *args, **kwargs):
+        qs = super().get_queryset(*args, **kwargs)
+        qs = qs.filter(user=self.request.user)
+        return qs
 
 class PlaceOrderView(View):
+    template_name = 'order/payment.html'
+
     def get(self, *args, **kwargs):
-        return HttpResponse("PlaceOrder")
+        if not self.request.user.is_authenticated:
+            messages.error(
+                self.request,
+                'You need to log in.'
+            )
+            return redirect('profile:create')
+        if not self.request.session.get('cart'):
+            messages.error(
+                self.request,
+                'Empty cart.'
+            )
+            return redirect('product:list')
+
+        cart = self.request.session.get('cart')
+        cart_variation_ids = [v for v in cart]
+        bd_variations = list(
+            Variation.objects.select_related('product')
+            .filter(id__in=cart_variation_ids)
+        )
+        for variation in bd_variations:
+            vid = str(variation.id)
+
+            stock = variation.stock
+            qtd_cart = cart[vid]['quantity']
+            unit_price = cart[vid]['unit_price']
+            promo_unit_price = cart[vid]['promotional_unit_price']
+
+            error_msg_stock = ''
+
+            if stock < qtd_cart:
+                cart[vid]['quantity'] = stock
+                cart[vid]['quantitative_pricing'] = stock * unit_price
+                cart[vid]['promotional_quantitative_pricing'] = stock * \
+                    promo_unit_price
+
+                error_msg_stock = 'Insufficient stock for some products in your cart. '\
+                    'We have reduced the quantity of these products. Please '\
+                    'check which products were affected below.'
+
+                if error_msg_stock:
+                    messages.error(
+                        self.request,
+                        error_msg_stock
+                    )
+
+                    self.request.session.save()
+                    return redirect('product:cart')
+
+        cart_total_qty = utils.cart_total_qty(cart)
+        cart_total_amount = utils.cart_totals(cart)
+
+        order = Order(
+            user=self.request.user,
+            total=cart_total_amount,
+            total_qty=cart_total_qty,
+            status='C',
+        )
+
+        order.save()
+
+        OrderItem.objects.bulk_create(
+            [
+                OrderItem(
+                    order=order,
+                    product=v['product_name'],
+                    product_id=v['product_id'],
+                    variation=v['variation_name'],
+                    variation_id=v['variation_id'],
+                    price=v['quantitative_pricing'],
+                    price_promo=v['promotional_quantitative_pricing'],
+                    quantity=v['quantity'],
+                    image=v['image'],
+                ) for v in cart.values()
+            ]
+        )
+
+        del self.request.session['cart']
+
+        return redirect(
+            reverse(
+                'order:payment',
+                kwargs={
+                    'pk': order.pk,
+                }
+            )
+        )
+
 class OrderDetailView(View):
     def get(self, *args, **kwargs):
         return HttpResponse("OrderDetail")
+
+
+class ListOrderView(View):
+    def get(self, *args, **kwargs):
+        return HttpResponse("List")
 
 
